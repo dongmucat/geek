bedfd4a8296315ee0760b6637bca4dc1cb836586
Songnian <1127093059@qq.com>
2022-07-17 17:30:28 +0800

    优化：使用了lazy路由懒加载

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@
 import { Route, Routes } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { HistoryRouter, history } from "./utils/history";
+import AuthComponent from "@/components/AuthComponent";
+// 导入必要组件
+import { lazy, Suspense } from "react";
+// 按需导入路由组件
+const Login = lazy(() => import("@/pages/Login"));
+const Layout = lazy(() => import("@/pages/Layout"));
+const Home = lazy(() => import("@/pages/Home"));
+const Article = lazy(() => import("@/pages/Article"));
+const Publish = lazy(() => import("@/pages/Publish"));
 // 导入页面组件
-import Login from "./pages/Login/index.js";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home/index.js";
-import Article from "./pages/Article/index.js";
-import Publish from "./pages/Publish/index.js";
-import AuthComponent from "@/components/AuthComponent/index.js";
+
 function App() {
 	return (
 		<HistoryRouter history={history}>
-			<div className="App">
+			<Suspense
+				fallback={
+					<div
+						style={{
+							textAlign: "center",
+							marginTop: 200,
+						}}
+					>
+						<h2>loading...</h2>
+					</div>
+				}
+			>
 				<Routes>
 					{/* 需要鉴权的路由 */}
 					<Route
@@ -25,13 +40,13 @@ function App() {
 					>
 						{/* 二级路由默认页面 */}
 						<Route index element={<Home />} />
-						<Route path="/article" element={<Article />} />
-						<Route path="/publish" element={<Publish />} />
+						<Route path="article" element={<Article />} />
+						<Route path="publish" element={<Publish />} />
 					</Route>
 					{/* 不需要鉴权的路由 */}
 					<Route path="/login" element={<Login />} />
 				</Routes>
-			</div>
+			</Suspense>
 		</HistoryRouter>
 	);
 }
